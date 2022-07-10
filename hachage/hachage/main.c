87a0a2642bb0451ff5bd989f5bea376c4a7453ac
Loacking <LudovicDop@gmail.com>
2022-07-10 14:33:31 +0200

    premier commit sans rien

---
+++ hachage/hachage/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  hachage
+//
+//  Created by Ludovic Doppler on 10/07/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
